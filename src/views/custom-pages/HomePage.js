ef211de0ef247c4be3844786c486477633198926
Mary Valera <emvalera356@gmail.com>
2024-05-05 19:44:54 +0800

    initial commit

---
+++ src/views/custom-pages/HomePage.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+// reactstrap components
+import {
+  Button,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Row,
+  Col
+} from "reactstrap";
+
+// core components
+import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
+import LandingPageHeader from "components/Headers/LandingPageHeader.js";
+import DefaultFooter from "components/Footers/DefaultFooter.js";
+
+function HomePage(){
+    const [firstFocus, setFirstFocus] = React.useState(false);
+    const [lastFocus, setLastFocus] = React.useState(false);
+
+}
+
+export default HomePage;
